dc9729bfe0994264c208decdb86d83e2bf96cbbb
Kevin Quintero <kevinstequintero@gmail.com>
2024-09-19 00:13:35 -0500

    Creacion del proyecto de spring boot
    se creo el proyecto de spring boot con las dependencias necesarias para iniciar

---
+++ cubiculos_backend/src/main/java/com/entornos/cubiculos_backend/CubiculosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.entornos.cubiculos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubiculosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubiculosBackendApplication.class, args);
+	}
+
+}
